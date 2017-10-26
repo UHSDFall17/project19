2d4c276d9b8ca65e97071145208cec265e7b7308
Kasey N <knknieman@gmail.com>
2017-10-26 14:12:15 -0500

    Added revised FileInOutTest.java

--- my-app/src/test/java/com/mycompany/app/FileInOutTest.java
+++ my-app/src/test/java/com/mycompany/app/FileInOutTest.java
@@ -6,19 +6,6 @@ import org.junit.Test;
 
 public class FileInOutTest {
 
-	@Test
-	public void testWriteToFile() {
-		FileInOut fileSystem = new FileInOut();
-		boolean test = fileSystem.writeToFile("testDoc.txt", "Test");
-		assertEquals(true, test);
-	}
-
-	@Test
-	public void testReplaceInFile() throws IOException{
-		FileInOut fileSystem = new FileInOut();
-		boolean test = fileSystem.replaceInFile("testDoc.txt", "Test", "test2");
-		assertEquals(true, test);
-	}
 
 	
 
