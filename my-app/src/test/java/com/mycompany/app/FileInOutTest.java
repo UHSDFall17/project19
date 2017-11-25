1471ef69ee926087fb8d99bd260f91dfc7f78e01
Prativa <devkotaprativa@gmail.com>
2017-11-25 14:51:32 -0600

    Test case added for checkForInvest

--- my-app/src/test/java/com/mycompany/app/FileInOutTest.java
+++ my-app/src/test/java/com/mycompany/app/FileInOutTest.java
@@ -11,6 +11,18 @@ public class FileInOutTest {
         boolean status = file.replaceInFile("PRATIVA.txt", "prativa", "Prativa");
         assertEquals(true, status);
     }
+    @Test
+    public void testCheckForFile() throws IOException{
+        FileInOut file = new FileInOut();
+        boolean status = file.checkForFile("PRATIVA.txt");
+        assertEquals(true, status);
+    }
+   @Test
+    public void testCheckForInvest() throws IOException{
+       FileInOut file = new FileInOut();
+       String[] word = file.checkForInvest("PRATIVA.txt");
+       assertEquals("11-25-2017", word[0]);
+   }
 
 
 	
