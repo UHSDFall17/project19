f4db1e94adb616cf89300a397c3facd85e56760f
Prativa <devkotaprativa@gmail.com>
2017-11-25 12:28:16 -0600

    test case added for ReplaceInFile

--- my-app/src/test/java/com/mycompany/app/FileInOutTest.java
+++ my-app/src/test/java/com/mycompany/app/FileInOutTest.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 import org.junit.Test;
 
 public class FileInOutTest {
+    @Test
+    public void testReplaceInFile() throws IOException{
+        FileInOut file = new FileInOut();
+        boolean status = file.replaceInFile("PRATIVA.txt", "prativa", "Prativa");
+        assertEquals(true, status);
+    }
 
 
 	
